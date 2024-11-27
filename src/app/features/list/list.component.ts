5f81aa4408faaa26ea3e0839137d22ca877d727f
Kayuan165 <kayuandiasoli@gmail.com>
2024-11-27 14:07:28 -0300

    feat: Adiciona funcionalidade de deletar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,7 +4,46 @@ import { Product } from '../../shared/interfaces/productInterface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { filter } from 'rxjs';
 
+@Component({
+  selector: 'app-confirmations-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+  template: `
+    <h2 mat-dialog-title>Deletar Produto</h2>
+    <mat-dialog-content>
+      Tem certeza que quer deletar esse produto?
+    </mat-dialog-content>
+    <mat-dialog-actions align="end">
+      <button mat-button (click)="onNo()">Não</button>
+      <button
+        mat-raised-button
+        color="primary"
+        (click)="onYes()"
+        cdkFocusInitial
+      >
+        Sim
+      </button>
+    </mat-dialog-actions>
+  `,
+})
+export class ConfirmationDialogComponent {
+  matDialogRef = inject(MatDialogRef);
+
+  onNo() {
+    this.matDialogRef.close(false);
+  }
+
+  onYes() {
+    this.matDialogRef.close(true);
+  }
+}
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -17,6 +56,7 @@ export class ListComponent {
 
   productService = inject(ProductsService);
   router = inject(Router);
+  matDialog = inject(MatDialog);
 
   ngOnInit() {
     this.productService.getAll().subscribe((products) => {
@@ -26,5 +66,19 @@ export class ListComponent {
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
+  }
+
+  onDelete(product: Product) {
+    this.matDialog
+      .open(ConfirmationDialogComponent)
+      .afterClosed()
+      .pipe(filter((answer) => answer === true))
+      .subscribe((answer: boolean) => {
+        this.productService.delete(product.id).subscribe(() => {
+          this.productService.getAll().subscribe((products) => {
+            this.products = products;
+          });
+        });
+      });
   }
 }
