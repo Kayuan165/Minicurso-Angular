b11786aad676d7266a79ce28024abfeccfe51de2
Kayuan165 <kayuandiasoli@gmail.com>
2024-12-04 13:41:01 -0300

    Revert "refactor: cria funções resolver especializadas"
    This reverts commit 6ce34b2daa8824fd937b4fc76d045f84197f1eb6.

--- src/app/shared/resolvers/get-products.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { ProductsService } from '../services/products.service';
-
-export const getProducts = () => {
-  const productsService = inject(ProductsService);
-
-  return productsService.getAll();
-};
